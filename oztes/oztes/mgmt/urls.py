c9dbce58a3df33d042e8ee46ddc1a3b5d624de12
air <kovalenkokv@gmail.com>
2023-01-26 18:41:29 +0300

    add rest api

--- oztes/oztes/mgmt/urls.py
+++ oztes/oztes/mgmt/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     #path('profile/', include('oztes.mgmt.registration.urls')),
     #path('profile/', views_registration.anonymous, name='anonymous registration'),
     path('profile/', views_registration.agent_list, name='anonymous profile'),
-    path('profile/<uuid:pk>/', views_registration.agent_detail, name='known user registration'),
+    path('profile/<uuid:pk>/', views_registration.agent_detail, name='known user   registration'),
     path('emp/', views_registration.emp_list, name='anonymous profile'),
 ]
